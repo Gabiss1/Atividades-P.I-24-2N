6411077436b6c42504a35314d9f8de38406fb2ff
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-04-16 21:53:11 -0300

    Add files via upload

--- aula-6/user.ts
+++ aula-6/user.ts
@@ -1,15 +1,47 @@
 export class User {
-    user_ID: number
-    user_Name: string
-    user_Address: string
-    user_Contact: string
-    user_Email: string
+    private user_ID: number
+    private user_Name: string
+    private user_Birth_Date: string
+    private user_Address: string
+    private user_Contact: string
+    private user_Email: string
+    private user_Specifications: string
 
-    constructor(id: number, name: string, address: string, contact: string, email: string){
+    constructor(id: number, name: string, birth_Date: string, address: string, contact: string, email: string, specifications: string){
         this.user_ID = id
         this.user_Name = name
+        this.user_Birth_Date = birth_Date
         this.user_Address = address
         this.user_Contact = contact
         this.user_Email = email
+        this.user_Specifications = specifications
+    }
+
+    getId():number{
+        return this.user_ID
+    }
+
+    getName():string{
+        return this.user_Name
+    }
+
+    getBirthDate():string{
+        return this.user_Birth_Date
+    }
+
+    getAddress():string{
+        return this.user_Address
+    }
+
+    getContact():string{
+        return this.user_Contact
+    }
+
+    getEmail():string{
+        return this.user_Email
+    }
+
+    getSpecifications():string{
+        return this.user_Specifications
     }
 }
